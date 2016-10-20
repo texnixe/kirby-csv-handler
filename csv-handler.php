000c3425afaed9d8cbffc679afa0331e39df2d36
texnixe <info@texniq.de>
2016-10-20 17:11:28 +0200

    Delete renamed page

--- csv-handler.php
+++
@@ -1,9 +0,0 @@
-<?php
-include __DIR__ . DS . 'routes.php';
-
-require_once __DIR__ . DS . 'csvimporter' . DS . 'csvimporter.php';
-require_once(__DIR__ . DS . 'lib' . DS . 'csv-handler.php');
-
-if (c::get('csv-handler.createpages.widget', false)) {
-      kirby()->set('widget', 'csv-handler.createpages', __DIR__ . DS . 'widgets' . DS . 'csv-handler.createpages');
-}
